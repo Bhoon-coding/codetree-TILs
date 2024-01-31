f62365445a69585a1c5e153a8c90ed05495e9b58
Byeonghoon Lee <64088377+Bhoon-coding@users.noreply.github.com>
2024-01-31 14:00:08 +0900

    Title: 입력받아 계산 2, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240131/입력받아 계산 2/input-calculate-2.swift
+++ 240131/입력받아 계산 2/input-calculate-2.swift
@@ -1,1 +1,8 @@
-print(13 * 17)
+// let input = Int(readLine()!)!
+let input = readLine()!.split(separator:" ").map { Int(String($0))! }
+
+
+var a = Int(input[0])
+var b = Int(input[1])
+
+print(a * b)
