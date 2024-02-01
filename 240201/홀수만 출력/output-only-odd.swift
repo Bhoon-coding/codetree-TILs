3690ce81056472a176aa0d0588b6895473ca09c8
Byeonghoon Lee <64088377+Bhoon-coding@users.noreply.github.com>
2024-02-01 23:28:51 +0900

    Title: 홀수만 출력, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240201/홀수만 출력/output-only-odd.swift
+++ 240201/홀수만 출력/output-only-odd.swift
@@ -2,7 +2,7 @@ var input = readLine()!.split(separator: " ").map { Int($0)! }
 var a = input[0]
 var b = input[1]
 var arr: [Int] = []
-for i in stride(from: a, to: b + 2, by: a + 1) {
+for i in stride(from: a, to: b + 1, by: a + 1) {
     arr.append(i)
 }
 print(arr.map { String($0) }.joined(separator: " "))
