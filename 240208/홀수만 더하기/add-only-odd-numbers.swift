3bd4332b5216cff447b005221e27a917f1c9799f
Byeonghoon Lee <64088377+Bhoon-coding@users.noreply.github.com>
2024-02-08 09:19:26 +0900

    Title: 홀수만 더하기, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240208/홀수만 더하기/add-only-odd-numbers.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+// 홀수이면서 3의배수인 요소의 합 reduce사용해보기
+var input: [Int] = []
+while let line = readLine(), let number = Int(line.trimmingCharacters(in: .whitespacesAndNewlines)) {
+    input.append(number)
+}
+
+let filteredArr = input.filter { $0.isMultiple(of: 3) && $0 % 2 == 1 }
+var result: Int = filteredArr.reduce(0) { $0 + $1 }
+print(result)
